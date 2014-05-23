92b93dc1a1031bbd962a5bad4cfce83db2eb78b8
Georg Nebehay <gnebehay@gmail.com>
2014-05-23 15:52:17 +0200

    Cleaned some code

--- exercises1/test.py
+++ exercises1/test.py
@@ -3,65 +3,63 @@ import ml
 
 if __name__ == '__main__':
 
-	#Read training/testing data
-	D = np.genfromtxt('training1.txt')
-	T = np.genfromtxt('testing.txt')
+	#Read artificial1 training/testing data
+	A = np.genfromtxt('artificial1-train.txt')
+	X_1 = A[:,:-1]
+	Y_1 = A[:,-1]
+	A = np.genfromtxt('artificial1-test.txt')
+	X_2 = A[:,:-1]
+	Y_2 = A[:,-1]
 
-	#Split into data and labels
-	Y = D[:,-1]
-	X = D[:,:-1]
-	T_Y = T[:,-1]
-	T = T[:,:-1]
+	#Perceptron
+	perc = ml.Perceptron()
+	E_1, E_2 = perc.train(X_1, Y_1, X_2, Y_2)
+	np.savetxt('artificial1-trainerror-perc.txt', E_1, fmt='%d')
+	np.savetxt('artificial1-testerror-perc.txt', E_2, fmt='%d')
 
-	p = ml.Perceptron()
-	ab = ml.AdaBoost()
+	#AdaBoost
+	boost = ml.AdaBoost()
+	E_1, E_2 = boost.train(X_1, Y_1, 100, X_2, Y_2)
+	np.savetxt('artificial1-trainerror-boost.txt', E_1, fmt='%d')
+	np.savetxt('artificial1-testerror-boost.txt', E_2, fmt='%d')
 
-	train_err, test_err = p.train(X,Y,T,T_Y)
-	np.savetxt('trainerror1-perc.txt', train_err, fmt='%d')
-	np.savetxt('testerror1-perc.txt', test_err, fmt='%d')
+	#Read artificial2 dataset
+	A = np.genfromtxt('artificial2-train.txt')
+	X_1 = A[:,:-1]
+	Y_1 = A[:,-1]
+    
+	#Perceptron
+	perc = ml.Perceptron()
+	E_1, E_2 = perc.train(X_1, Y_1, X_2, Y_2)
+	np.savetxt('artificial2-trainerror-perc.txt', E_1, fmt='%d')
+	np.savetxt('artificial2-testerror-perc.txt', E_2, fmt='%d')
 
-	train_err, test_err = ab.train(X,Y,10,T,T_Y)
-	np.savetxt('trainerror1-boost.txt', train_err, fmt='%d')
-	np.savetxt('testerror1-boost.txt', test_err, fmt='%d')
+	#AdaBoost
+	boost = ml.AdaBoost()
+	E_1, E_2 = boost.train(X_1, Y_1, 100, X_2, Y_2)
+	np.savetxt('artificial2-trainerror-boost.txt', E_1, fmt='%d')
+	np.savetxt('artificial2-testerror-boost.txt', E_2, fmt='%d')
 
-	#Reset classifiers
-	p = ml.Perceptron()
-	ab = ml.AdaBoost()
-
-	D = np.genfromtxt('training2.txt')
-
-	##Split into data and labels
-	Y = D[:,-1]
-	X = D[:,:-1]
-
-	train_err, test_err = p.train(X,Y,T,T_Y)
-	np.savetxt('trainerror2-perc.txt', train_err, fmt='%d')
-	np.savetxt('testerror2-perc.txt', test_err, fmt='%d')
-
-	train_err, test_err = ab.train(X,Y,100,T,T_Y)
-	np.savetxt('trainerror2-boost.txt', train_err, fmt='%d')
-	np.savetxt('testerror2-boost.txt', test_err, fmt='%d')
-
-	#Read training/testing data
-	wine_train = np.genfromtxt('wine-train.txt')
-	wine_test = np.genfromtxt('wine-test.txt')
+	#Wine dataset
+	A = np.genfromtxt('wine-train.txt')
+	X_1 = A[:,1:]
+	Y_1 = A[:,0]
+	A = np.genfromtxt('wine-test.txt')
+	X_2 = A[:,1:]
+	Y_2 = A[:,0]
 
 	#Create multiclass problem
-	wine_labels_train = wine_train[:,0]
-	wine_data_train = wine_train[:,1:]
-	wine_labels_test = wine_test[:,0]
-	wine_data_test = wine_test[:,1:]
-
 	boosts = []
 	replacements = []
-	for c in np.unique(wine_labels_train):
-		new_labels = wine_labels_train.copy()
-		new_labels[wine_labels_train == c] = 1
-		new_labels[wine_labels_train != c] = -1
+	for c in np.unique(Y_1):
+		new_labels = Y_1.copy()
+		new_labels[Y_1 == c] = 1
+		new_labels[Y_1 != c] = -1
 
 		#Train AdaBoost
 		b = ml.AdaBoost()
-		b.train(wine_data_train, new_labels, 100, wine_data_test, wine_labels_test)
+		import ipdb; ipdb.set_trace()
+		b.train(X_1, new_labels, 100, X_2, Y_2)
 
 		replacements.append(c)
 
@@ -72,11 +70,11 @@ if __name__ == '__main__':
 
 	#Multiclass classification
 	#This is quite ugly
-	confidence = np.vstack([b.classify(wine_data_test)[1] for b in boosts])
+	confidence = np.vstack([b.classify(X_2)[1] for b in boosts])
 
 	max_conf = np.argmax(confidence, axis=0)
 	best_label = replacements[max_conf]
-	wine_test_error = np.sum(best_label != wine_labels_test)
+	wine_test_error = np.sum(best_label != Y_2)
 
 	print 'wine test error:' , wine_test_error
 
